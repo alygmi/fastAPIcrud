a4561ed93f8f65a93c54e4f994a28dfd79e88b2f
alygmi <nurrusulialadiyat@gmail.com>
2025-04-11 10:37:12 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# fastAPIcrud
---
+++ models.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, String, Text, ForeignKey
+from sqlalchemy.orm import relationship
+from .database import Base
+
+
+class jabatan(Base):
+    __tablename__ = "jabatan"
+
+    id_jabatan = Column(Integer, primary_key=True, index=True)
+    nama_jabatan = Column(String(100))
+    deskripsi = Column(Text)
+
+    users = relationship("User", back_populates="jabatan")
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id_user = Column(Integer, primary_key=True, index=True)
+    nama = Column(String(255))
+    alamat = Column(Text)
+    telepon = Column(String(20))
+    id_jabatan = Column(Integer, ForeignKey("jabatan.id_jabatan"))
+
+    jabatan = relationship("jabatan", back_populates="users")
