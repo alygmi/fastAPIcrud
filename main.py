4b5310db4c83a61706ebb14b0caab5a8e4a5fc82
alygmi <nurrusulialadiyat@gmail.com>
2025-04-11 16:34:41 +0700

    adding endpoint for get all users

--- main.py
+++ main.py
@@ -50,6 +50,13 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
 
 @app.get("/users/", response_model=List[schemas.User])
+def read_all_user(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = db.query(models.User).offset(skip).limit(limit).all()
+    return users
+
+
+# endpoint get user by id
+@app.get("/users/{user_id}", response_model=List[schemas.User])
 def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = db.query(models.User).filter(
         models.User.id_user == user_id).first()
